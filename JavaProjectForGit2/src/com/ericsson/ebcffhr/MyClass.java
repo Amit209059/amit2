ca9f4dcfef6a2710346eaacfb31a96428ab08a72
Amit209059 <kieshankumar@gmail.com>
2019-05-02 04:31:50 +0530

    Created MyClass2 and then called it from Myclass using creating object

--- JavaProjectForGit2/src/com/ericsson/ebcffhr/MyClass.java
+++ JavaProjectForGit2/src/com/ericsson/ebcffhr/MyClass.java
@@ -4,7 +4,10 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("My Name is Amit");
+		System.out.println("Class 1");
+		
+		MyClass2 mc2 = new MyClass2();
+		mc2.printName();
 
 	}
 
---
+++ JavaProjectForGit2/src/com/ericsson/ebcffhr/MyClass2.java
@@ -0,0 +1,8 @@
+package com.ericsson.ebcffhr;
+
+public class MyClass2 {
+	public void printName() {
+		System.out.println("Class 2");
+	}
+
+}
